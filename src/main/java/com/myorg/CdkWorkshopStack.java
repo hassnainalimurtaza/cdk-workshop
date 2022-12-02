685ca129957a141099d000734c7bb8e949fb7a4a
Hassnain Ali Murtaza <michael.jackson4546@gmail.com>
2022-12-03 02:41:58 +0500

    cdk-project-java
    cdk project with sqs,sns and lamda implementation

--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,12 +1,16 @@
 package com.myorg;
 
+
 import software.constructs.Construct;
-import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
+
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+
+
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
 
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
@@ -16,14 +20,13 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
+        // Defines a new lambda resource
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_14_X)    // execution environment
+                .code(Code.fromAsset("lambda"))  // code loaded from the "lambda" directory
+                .handler("message.handler")        // file is "hello", function is "handler"
                 .build();
-
-        final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
-
-        topic.addSubscription(new SqsSubscription(queue));
     }
+
+
 }
